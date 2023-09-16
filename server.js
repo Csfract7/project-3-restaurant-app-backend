1c63b145eef1d99a17cd7697bc716e2fae5f76db
csfract <Csfractg@gmail.com>
2023-09-16 11:32:51 -0700

    Set up

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const { PORT } = process.env;
+const express = require("express");
+const app = express();
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+
+app.get("/", (req, res) => {
+  res.send("sanity check");
+});
+
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
